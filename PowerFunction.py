b218dee38251e7fc792ca9ae613a319703e70fa6
Darlene Fung <Darlene@Darlenes-MBP.westell.com>
2016-03-09 19:21:57 -0500

    final program

--- PowerFunction.py
+++ PowerFunction.py
@@ -1,47 +1,23 @@
-
-
-
 # Darlene Fung
 # 3/8/16
 
-#	""" Takes a base and exponent and calculates the value of base raised to the power of the integer """
+''' Takes a base and exponent and calculates the value of base raised to the power of the integer '''
 
-
-
-#def plus_ten(x):
-#	return x + 10
-#print plus_ten(44)
-
-
-	
-
-
-def power(base, exponent): 
-	counter = 1
-	rememberNumber = base
-	if exponent == 0:
+def power(base, exponent):  # takes in 2 variables, a base and an exponent
+	counter = 1				# keeps track of how many times the base as been multiplied
+	rememberNumber = base	# when program starts, the number you are multiplying is the base
+	if exponent == 0:		# if the exponent is 0, the answer is always 1
 		return 1
-	while counter != exponent: 
-		rememberNumber = rememberNumber * base 
-		counter += 1
+	while counter != exponent: 	# if the exponent is not equal to the counter (so is not equal to 1, because the counter is set to 1 initially) 
+		rememberNumber = rememberNumber * base # the new number being multiplied is compiled
+		counter += 1		# counter increases by 1 to keep track of the number times the base has been multiplied
+	return rememberNumber 	# when loop ends, it returns the most recent rememberNumber, which is the final answer 
 	
-	return rememberNumber 
-	
-base = int(raw_input("What is your base? "))
-exponent = int(raw_input("What is your exponent? "))
-	
-print power(base, exponent)
+base = int(raw_input("What is your base? "))			# asks for base
+exponent = int(raw_input("What is your exponent? "))	# asks for exponent
+
+print("power({}, {}) -> {}".format(base, exponent, power(base,exponent))) # prints you base, exponent, and what the answer is
 
 
-#currentValue = base
 
-#if exponent == 1:
-#	print(base) 
-	
-#while exponent != 1 and exponent != 0: 
-#while counter != exponent = 1:
-#	rememberNumber = currentValue
-#	currentValue = rememberNumber * base
-#	counter += 1
-	
 	
